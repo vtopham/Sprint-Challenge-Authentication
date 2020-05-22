7e43007de363d7ff662ad42f7f0c6fc9da28a7f7
Victoria  Topham <topham.victoria@gmail.com>
2020-05-22 09:40:13 -0700

    got routing working on my app

---
+++ dad-jokes-app/src/Components/jokes/Jokes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Jokes = () => {
+    return (
+        <>
+            <h1>This is the jokes page</h1>
+        </>
+    )
+}
+
+export default Jokes;
---
+++ dad-jokes-app/src/Components/sign_in/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <>
+            <h1>This is the signin page</h1>
+        </>
+    )
+}
+
+export default SignIn;
