f71d2cf46d9e356aeb3d0e067622eab5d748d9fb
Victoria  Topham <topham.victoria@gmail.com>
2020-05-22 09:54:34 -0700

    you can succcessfully register using the app

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
@@ -11,6 +12,7 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
--- dad-jokes-app/src/Components/sign_up/SignUp.js
+++ dad-jokes-app/src/Components/sign_up/SignUp.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+
+const StyledDiv = styled.div`
+
+
+
+`
 
 const SignUp = () => {
+
+    const [formState, setFormState] = useState({username: "", password: ""});
+
+    const updateFormState = event => {
+        setFormState({...formState, [event.target.name]: event.target.value})
+    }
+
+    const submitForm = event => {
+        event.preventDefault();
+        axios.post(`http://localhost:3300/api/auth/register`, formState)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     return (
-        <>
-            <h1>This is the signup page</h1>
-        </>
+        <StyledDiv>
+            <h1>Welcome! Please sign up.</h1>
+            <form onSubmit = {submitForm}>
+                <div className = 'input-container'>
+                    <label htmlFor = "username" name = "username" >Username: </label>
+                    <input name = "username" onChange = {updateFormState} value = {formState.username}/>
+                </div>
+                <div className = 'input-container'>
+                    <label htmlFor = "password" name = "password">Password: </label>
+                    <input type = "password" name = "password" onChange = {updateFormState} value = {formState.password}/>
+                </div>
+                <button>Submit</button>
+            </form>
+        </StyledDiv>
     )
 }
 
