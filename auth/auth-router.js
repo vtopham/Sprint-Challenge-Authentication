8c80b910f5bfce99c6ee240e6331b8c38b4624a4
Victoria  Topham <topham.victoria@gmail.com>
2020-05-22 08:12:01 -0700

    added an endpoint to see if posting users worked

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 // /api/auth
 
 const router = require('express').Router();
+const Users = require('./auth-model.js')
 
 router.post('/register', (req, res) => {
   // implement registration
@@ -9,6 +10,16 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   // implement login
 });
+
+router.get('/users', (req, res) => {
+    Users.getUsers()
+      .then(users => {
+        res.status(200).json({data: users})
+      })
+      .catch(Err => {
+        res.status(500).json({message: "Error retrieving users", error: err})
+      })
+})
 
 function validateUserInfo(req, res, next) {
   if (req.body && req.body.username && req.body.password) {
