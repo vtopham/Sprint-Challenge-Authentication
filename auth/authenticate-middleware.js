d5432bff96bdb4775b734ae8e493268b509bce3b
Victoria  Topham <topham.victoria@gmail.com>
2020-05-22 08:30:59 -0700

    implemented the authenticate middleware inside the file

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,19 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
-
+const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  if (req.headers.authorization) {
+    const token = req.headers.authorization;
+    jwt.verify(token, "banana", (error, decodedToken) => {
+      if(!error) {
+        req.jwt = decodedToken
+        next();
+      } else {
+        res.status(400).json({message: "Your token is invalid"})
+      }
+    })
+  } else {
+    res.status(400).json({message: "Please include your authentication token in the header."})
+  }
 };
