f2bb50815cb86b3bae12c02f49d743dd57c0b0f1
Victoria  Topham <topham.victoria@gmail.com>
2020-05-22 10:13:05 -0700

    can retreive jokes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -61,15 +61,15 @@ router.post('/login', (req, res) => {
   
 });
 
-// router.get('/users', (req, res) => {
-//     Users.getUsers()
-//       .then(users => {
-//         res.status(200).json({data: users})
-//       })
-//       .catch(Err => {
-//         res.status(500).json({message: "Error retrieving users", error: err})
-//       })
-// })
+router.get('/users', (req, res) => {
+    Users.getUsers()
+      .then(users => {
+        res.status(200).json({data: users})
+      })
+      .catch(Err => {
+        res.status(500).json({message: "Error retrieving users", error: err})
+      })
+})
 
 function validateUserInfo(req, res, next) {
   if (req.body && req.body.username && req.body.password) {
--- dad-jokes-app/src/Components/jokes/Jokes.js
+++ dad-jokes-app/src/Components/jokes/Jokes.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import axios from 'axios'
+
+
 
 const Jokes = () => {
+
+    const token = localStorage.getItem('token')
+    const authenticatedAxios = axios.create({
+        baseURL: 'http://localhost:3300',
+        headers: {Authorization: token}
+    })
+
+    const [jokes, setJokes] = useState([])
+    
+
+    useEffect( _ => {
+        authenticatedAxios.get(`/api/jokes`)
+            .then(response => {
+                console.log(response)
+                setJokes(response.data)
+            })
+            .catch(err => {
+                        console.log(err)
+                    })
+    },[])
+
+    // if(!token) {
+    //     return(
+    //         <>
+    //         <h1>Please Log In.</h1>
+    //         </>
+    //     )
+    // }
+    
+
+    
+
     return (
         <>
             <h1>This is the jokes page</h1>
+            
         </>
     )
 }
